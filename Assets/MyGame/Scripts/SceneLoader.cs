00bef48ca0cf9476139fd9b151821cde29bc7fba
smeerws <hello@xrux.at>
2018-12-17 09:36:48 +0100

    setup sprites and ui elements in game over scene

---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    private const string SCENEWELCOME     = "Welcome";
+    private const string SCENEGAMEOVER    = "GameOver";
+    private const string SCENEGROWMANMAIN = "GrowmanMain";
+
+    void LoadMainGame()
+    {
+        SceneManager.LoadScene(SCENEGROWMANMAIN);
+    }
+
+    void LoadWelcome()
+    {
+        SceneManager.LoadScene(SCENEWELCOME);
+    }
+
+    void LoadGameOver()
+    {
+        SceneManager.LoadScene(SCENEGAMEOVER);
+    }
+}
